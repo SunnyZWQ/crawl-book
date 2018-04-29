fedf485a5cdb001113bb3ac17b298e0883f51f46
zhangwenqing <zhangwenqing@unitedstack.com>
2018-04-29 16:03:12 +0800

    add comment

--- tutorial/spiders/testComment.py
+++ tutorial/spiders/testComment.py
@@ -13,7 +13,22 @@ class doubanSpider(scrapy.Spider):
     def parse(self, response):
         # reload(sys)
         # sys.setdefaultencoding('utf-8')
-        comment = response.xpath('//*[@id="comments"]/ul/li[1]/div[2]').extract()
+        user = response.xpath('//*[@id="comments"]/ul/li[1]/div[2]/h3/span[2]/a/text()').extract()
+        rate = response.xpath('//*[@id="comments"]/ul/li[1]/div[2]/h3/span[2]/span/@title').extract()[0]
 
         with open('comment.txt','w') as f:
-            f.write(str(comment))
+            f.write(user + '  ' + rank(rate) + '  ')
+
+
+    def rank(level):
+        if level=='力荐'
+            return 5
+        if level=='推荐'
+            return 4
+        if level=='还行'
+            return 3
+        if level=='较差'
+            return 2
+        if level=='很差'
+            return 1
+        
