9d7808fddc245bbfc70e758af862470baabff278
zhangwenqing <zhangwenqing@unitedstack.com>
2018-04-29 15:06:42 +0800

    add entry

--- tutorial/spiders/testEntry.py
+++ tutorial/spiders/testEntry.py
@@ -4,6 +4,7 @@ import sys
 
 
 class doubanSpider(scrapy.Spider):
+    
     name = 'testEntry'
 
     start_urls = [
@@ -11,12 +12,22 @@ class doubanSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        #reload(sys)
-        #sys.setdefaultencoding('utf-8')
-        #a = response.xpath('//*[@id="info"]/a[1]').extract().strip().replace('<*>','')
+        # reload(sys)
+        # sys.setdefaultencoding('utf-8')
         author = response.xpath('//*[@id="info"]/a[1]/text()').extract()[0].strip().replace('\n','').replace(' ','')
         public = response.xpath('//*[@id="info"]/text()[5]').extract()[0].replace(' ','')
+        origin_name = response.xpath('//*[@id="info"]/text()[7]').extract()[0].replace(' ','')
+        public_year = response.xpath('//*[@id="info"]/text()[10]').extract()[0].replace(' ','')
+        pages = response.xpath('//*[@id="info"]/text()[12]').extract()[0].replace(' ','')
+        price = response.xpath('//*[@id="info"]/text()[14]').extract()[0].replace(' ','')
+        book_type = response.xpath('//*[@id="info"]/text()[16]').extract()[0].replace(' ','')
+        isbn =  response.xpath('//*[@id="info"]/text()[20]').extract()[0].replace(' ','')
+
         with open('entry.txt','w') as f:
-            f.write(author + '  ' + public)
+            f.write(author + '  ' + public + '  ' + origin_name + '  ' + public_year \
+                    + '  ' + pages + '  ' + price + '  ' + book_type + '  ' + isbn)
+
+
+
 
 
