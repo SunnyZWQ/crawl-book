8fb010b024f1dc4fc929ebbacfad68f8b4187b26
zhangwenqing <zhangwenqing@unitedstack.com>
2018-04-29 16:19:44 +0800

    .

--- tutorial/spiders/testComment.py
+++ tutorial/spiders/testComment.py
@@ -16,21 +16,21 @@ class doubanSpider(scrapy.Spider):
 
         def rank(level):
             if level=='力荐':
-                return 5
+                return '5'
             if level=='推荐':
-                return 4
+                return '4'
             if level=='还行':
-                return 3
+                return '3'
             if level=='较差':
-                return 2
+                return '2'
             if level=='很差':
-                return 1
+                return '1'
 
         user = response.xpath('//*[@id="comments"]/ul/li[1]/div[2]/h3/span[2]/a/text()').extract()[0]
         rate = response.xpath('//*[@id="comments"]/ul/li[1]/div[2]/h3/span[2]/span/@title').extract()[0]
         rate = str(rank(rate))
         with open('comment.txt','w') as f:
-            f.write(user + '  ' + rank(rate) + '  ')
+            f.write(str(user) + '  ' + rate)
 
 
         
