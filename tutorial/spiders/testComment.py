3787aaee6ae682d533b047d1afa8067886d38908
zhangwenqing <zhangwenqing@unitedstack.com>
2018-04-29 15:19:42 +0800

    add comment

---
+++ tutorial/spiders/testComment.py
@@ -0,0 +1,19 @@
+import scrapy
+import sys
+
+
+class doubanSpider(scrapy.Spider):
+    
+    name = 'testComment'
+
+    start_urls = [
+        'https://book.douban.com/subject/25862578/comments/'
+    ]
+
+    def parse(self, response):
+        # reload(sys)
+        # sys.setdefaultencoding('utf-8')
+        comment = response.xpath('//*[@id="comments"]/ul/li[1]/div[2]').extract()
+
+        with open('comment.txt','w') as f:
+            f.write(comment)
