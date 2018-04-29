d96a622e5484f1ca9bbb6b2765d58ec21c1fae48
zhangwenqing <1097201000@qq.com>
2018-04-29 11:27:25 +0800

    initial

---
+++ tutorial/spiders/testEntry.py
@@ -0,0 +1,21 @@
+import scrapy
+import sys
+#from my.items import MyscrapyItem,bookLink,book
+
+
+class doubanSpider(scrapy.Spider):
+    name = 'testEntry'
+
+    start_urls = [
+        'https://book.douban.com/subject/25862578/'
+    ]
+
+    def parse(self, response):
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
+        a = response.xpath('//*[@id="info"]/a[1]').extract().strip().replace('<*>','')
+	with open('entry.txt','w') as f:
+	    f.write(a)
+	print(a)
+        print('hahahahahahahahhhahahahhhaha')
+
