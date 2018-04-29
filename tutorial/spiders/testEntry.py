f2ab8a502830211105a0614a0802cdffc82647af
zhangwenqing <zhangwenqing@unitedstack.com>
2018-04-29 14:35:15 +0800

    ..

--- tutorial/spiders/testEntry.py
+++ tutorial/spiders/testEntry.py
@@ -14,12 +14,8 @@ class doubanSpider(scrapy.Spider):
         #reload(sys)
         #sys.setdefaultencoding('utf-8')
         #a = response.xpath('//*[@id="info"]/a[1]').extract().strip().replace('<*>','')
-<<<<<<< HEAD
-        a = response.xpath('//*[@id="info"]/a[1]/text()').extract()[0].strip().replace('\n','').replace(' ','')
-=======
         author = response.xpath('//*[@id="info"]/a[1]/text()').extract()[0].strip().replace('\n','').replace(' ','')
         public = response.xpath('//*[@id="info"]/text()[2]').extract()[0].strip().replace('\n','').replace(' ','')
->>>>>>> ed0929a470979bf21f3971095b1487a7ec5bc53c
         with open('entry.txt','w') as f:
             f.write(author + '  ' + public)
 
