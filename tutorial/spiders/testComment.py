28fa8f286399fadf4e5b1fc581a8986b83e9ef7a
zhangwenqing <zhangwenqing@unitedstack.com>
2018-04-30 14:39:55 +0800

    .

--- tutorial/spiders/testComment.py
+++ tutorial/spiders/testComment.py
@@ -30,15 +30,15 @@ class doubanSpider(scrapy.Spider):
         comments = response.xpath('//*[@id="comments"]/ul/li')
         i = 1
         page = 1
-        while(i < 20):
-        # for item in comments:
-            user = comments[i].xpath('.//div[2]/h3/span[2]/a/text()').extract()[0]
-            rate = comments[i].xpath('.//div[2]/h3/span[2]/span[1]/@title').extract()[0]
-            date = comments[i].xpath('.//div[2]/h3/span[2]/span[2]/text()').extract()[0]
-            rate = str(rank(rate))
-            with open('comment.txt','a') as f:
+        with open('comment.txt','w') as f:
+            while(i < 20):
+            # for item in comments:
+                user = comments[i].xpath('.//div[2]/h3/span[2]/a/text()').extract()[0]
+                rate = comments[i].xpath('.//div[2]/h3/span[2]/span[1]/@title').extract()[0]
+                date = comments[i].xpath('.//div[2]/h3/span[2]/span[2]/text()').extract()[0]
+                rate = str(rank(rate))
                 f.write(book + '  ' + str(user) + '  ' + rate + '  ' + date + '\n')
-            i = i + 1
+                i = i + 1
 
         next_page = response.xpath('//*[@id="content"]/div/div[1]/div/div[3]/ul/li[3]/a/@href').extract()[0]
         next_page = self.start_urls[0] + next_page
