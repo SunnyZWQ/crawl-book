aeafed9cd2ded3f5b94b457cee075189e29c9069
zhangwenqing <zhangwenqing@unitedstack.com>
2018-05-01 14:36:20 +0800

    .

--- tutorial/spiders/testLink.py
+++ tutorial/spiders/testLink.py
@@ -39,7 +39,7 @@ class doubanSpider(scrapy.Spider):
                 next_page = response.urljoin(next_page)
                 yield scrapy.Request(next_page, callback=self.parse_list)
         except Exception:
-            response.xpath('//*[@id="subject_list"]/div[2]/span[3]/a/@href').extract()[0]
+            next_page = response.xpath('//*[@id="subject_list"]/div[2]/span[3]/a/@href').extract()[0]
             next_page = 'https://book.douban.com' + next_page
             if next_page is not None:
                 next_page = response.urljoin(next_page)
