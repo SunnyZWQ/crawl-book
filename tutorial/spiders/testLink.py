fd4df7a33e0900219711c288872ad2285c3681dd
zhangwenqing <zhangwenqing@unitedstack.com>
2018-05-01 14:19:07 +0800

    .

--- tutorial/spiders/testLink.py
+++ tutorial/spiders/testLink.py
@@ -30,7 +30,7 @@ class doubanSpider(scrapy.Spider):
             link = i.xpath('.//div[2]/h2/a').extract()[0]
             link = response.urljoin(link)
             # booklist --> entry
-            yield scrapy.Request(next_page, callback=self.parse_entry)
+            yield scrapy.Request(link, callback=self.parse_entry)
         
         next_page = response.xpath('//*[@id="subject_list"]/div[2]/span[4]/a/@href').extract()[0]
         next_page = 'https://book.douban.com' + next_page
